1a9fff13475f76ee82c76d03991648de432ec9c4
nick9999 <nikhilpatil123@gmail.com>
2014-05-24 23:33:06 +0530

    Create cubefr.cpp

---
+++ cubefr.cpp
@@ -0,0 +1,70 @@
+#include<stdio.h>
+char ina[1000001];
+int ar[1000001];
+int main()
+{
+	int t,i,j,temp,sol,in,fi,ans,mi,n;
+	scanf("%d",&t);
+	for(i=2;i<101;i++)
+	{
+		temp=i*i*i;
+		j=temp;
+		while(temp<1000001)
+		{
+			ina[temp]=1;
+			temp=temp+j;
+		}
+	}
+		ans=2;
+		ar[0]=0;ar[1]=1;
+		for(i=2;i<1000001;i++)
+	{
+	
+		if(ina[i]!=1)
+			ar[ans++]=i;
+	}
+	for(j=1;j<=t;j++)
+	{
+		scanf("%d", &n);
+		if(ina[n]==1)
+			printf("Case %d: Not Cube Free\n", j);
+		else
+		{
+			for(i=0;i<ans;i++)
+			{
+				in =0;
+				fi = ans;
+				while(in<=fi)
+				{
+					if(in==fi)
+					{
+						if(ar[in]==n)
+						{
+							sol= in;
+							break;
+						}
+					}
+					else
+					{
+						mi = (in+fi)/2;
+						if(ar[mi]==n)
+						{
+							sol = mi;
+							break;
+						}
+						else if(ar[mi]<n)
+							in = mi +1;
+						else 
+							fi = mi-1;
+					}
+				}
+				printf("Case %d: %d\n", j, sol);
+				break;
+			}
+		}
+	}
+	return 0;
+}
+		
+	
+
