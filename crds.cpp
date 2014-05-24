14709aa4c4ad9db5741d749dcb0347b1c55f04e6
nick9999 <nikhilpatil123@gmail.com>
2014-05-24 23:32:20 +0530

    Create crds.cpp

---
+++ crds.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    long long n,ans;
+    while(t--)
+    {
+        scanf("%lld",&n);
+        ans=n*(n+1)/2;
+        ans=ans*3-n;
+        ans%=1000007;
+        printf("%lld\n",ans);
+
+    }
+
+
+    return 0;
+}
