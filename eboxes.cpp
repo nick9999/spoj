f9bc1bd25f8c9c26a9a4c1a5073021c882d28200
nick9999 <nikhilpatil123@gmail.com>
2014-05-24 23:35:40 +0530

    Create eboxes.cpp

---
+++ eboxes.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+	int cs, n, k, t, f;
+	scanf("%d", &cs);
+	while(cs--) {
+		scanf("%d %d %d %d", &n, &k, &t, &f);
+		printf("%d\n", (f - n) / (k - 1) + f);
+	}
+	return 0;
+}
